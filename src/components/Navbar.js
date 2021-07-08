c179007cc6740db9e583615e0df3c9a1cf3e7c43
Heatwave Eli <drcharan2007@gmail.com>
2021-07-08 16:50:57 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <div className="navbar">
       <h1 className="nav-name">Heatwave Eli</h1>
       <div className="separator"></div>
-      <a className="justmrbhd-link" rel="noreferrer" href="https://justmrbhddev.glitch.me">JUSTMRBHD's Portfolio</a>
+      <a className="justmrbhd-link" rel="noreferrer" href="https://justmrbhddev.netlify.app">JUSTMRBHD's Portfolio</a>
     </div>
   );
 }
